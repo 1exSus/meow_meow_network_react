24bad856639317c31dea64f0d0468cc1353fca7c
1exSus <leus-maksim@mail.ru>
2023-02-13 18:43:28 +0300

    test2

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ let rerenderEntireTree = () => {
 rerenderEntireTree(store.getState());
 store.subscriber(rerenderEntireTree)
 
-//test
+//test2
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
