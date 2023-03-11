8759eeece3e1193b915442ed604ef6f05f7f082a
1exSus <leus-maksim@mail.ru>
2023-03-11 03:31:13 +0300

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -10,15 +10,12 @@ import {Provider} from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
         </BrowserRouter>
-    </React.StrictMode>
 );
-
 
 //test2
 
